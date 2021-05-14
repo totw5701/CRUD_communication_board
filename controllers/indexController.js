827f80f51fd11ba92216b3fdfdf4e0bf5c2fd3a7
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-05-14 20:02:51 +0900

    검색기능 추가

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -46,14 +46,77 @@ export const home = (req, res) => {
       </div>
 
       <div class="searchBox__frame">
-        <div class="searchBox">
+        <form class="searchBox" action="/search" method="get">
             <input class="searchPost__input" type="text" name="searchPost">
             <button class="searchPost__button" type="submit"><i class="fas fa-search"></i></button>
-        </div>
+        </form>
       </div>
       `,
       ``
       ) 
       res.send(html);
     })
+}
+
+export const search = (req, res) => {
+  const {
+    query: { searchPost: searchingBy }
+  } = req;
+
+  console.log(searchingBy);
+
+
+  db.query(`SELECT posts.id, title,description,created,nickName,created FROM posts JOIN authors ON posts.author = authors.id  WHERE title like "%${searchingBy}%" ORDER BY posts.id`, 
+  function(err, posts){
+
+    console.log("search!",posts);
+    if(!posts){
+      posts = [];
+    }
+    
+    const authentication = auth.statusUI(req, res);
+
+    //pages
+    const _url = req.url;
+    const queryData = url.parse(_url, true).query;
+    const page_query = queryData.page_num;
+
+    const page_select = template.PAGES_SELECTOR(posts, page_query);
+
+    let list = template.LIST(posts, page_query);
+    let html = template.HTML("Welcome",
+    authentication,
+    `
+    <div class="post__top-bar">
+      <span class="post__top-bar__num"=>글번호</span>
+      <span class="post__top-bar__title">제목</span>
+      <span class="post__top-bar__date">날짜</span>
+      <span class="post__top-bar__author">작성자</span>
+    </div>
+
+    <div class="post__list">
+      ${list}
+    </div>
+
+    <div class="post__bottom-bar">
+      <div></div>
+      <div class="post__page-num">
+        ${page_select}
+      </div>
+      <div>
+        <a href="/posts/create" class="control__fram__button">CREATE</a>            
+      </div>
+    </div>
+
+    <div class="searchBox__frame">
+      <form class="searchBox" action="/search" method="get">
+          <input class="searchPost__input" type="text" name="searchPost">
+          <button class="searchPost__button" type="submit"><i class="fas fa-search"></i></button>
+      </form>
+    </div>
+    `,
+    ``
+    ) 
+    res.send(html);
+  })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-const { home } = require('../controllers/indexController');
+const { home, search } = require('../controllers/indexController');
 
 
 
 router.get('/', home)
+router.get('/search', search)
 
 export default router;
 //module.exports = router;
